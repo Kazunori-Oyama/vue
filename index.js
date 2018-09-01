39ec7e7e21a4ef06b80eb3f92bb5c578b826f29e
KazunoriOyama <kazunori@MBA.local>
2018-09-01 22:06:39 +0900

    learn component fundamental

--- index.js
+++ index.js
@@ -48,3 +48,19 @@ var app6 = new Vue({
     inputMessage:'Hello '
   }
 })
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app = new Vue({
+  el: '#app7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
