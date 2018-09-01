5d1b66218d18d9f73b86281e6d87e2161c2e4bc5
KazunoriOyama <kazunori@MBA.local>
2018-09-01 13:56:15 +0900

    learn "for"directive

--- index.js
+++ index.js
@@ -4,9 +4,28 @@ var app = new Vue({
       message: 'Hello Vue!'
     }
   })
+
 var app2 = new Vue({
   el: '#app-2',
   data:{
-    message:'some message'
+    mydata:'I love Gin and Tonic'
+  }
+})
+
+var app3 = new Vue({
+  el:'#app-3',
+  data:{
+    iCanControlIt:false
+  }
+})
+
+var app4 = new Vue({
+  el:'#app-4',
+  data:{
+    todos:[
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
   }
 })
